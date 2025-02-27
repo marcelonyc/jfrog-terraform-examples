999460685cb7b9ab0d53f27cf9c71726fcdad8c8
marcelonyc <marcelo.litovsky@gmail.com>
2025-02-27 09:53:27 -0500

    Corrected indent

--- wm-federation-test/main.tf
+++ wm-federation-test/main.tf
@@ -14,7 +14,7 @@ resource "artifactory_federated_maven_repository" "terraform-federated-test-mave
 
   }
 
-    member {
+  member {
     url     = "${var.JFROG_URL_SOLENG}/artifactory/maven-dev-repo"
     access_token = var.JFROG_ACCESS_TOKEN_SOLENG
     enabled = true
